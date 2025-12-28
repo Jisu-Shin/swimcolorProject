bcefb402b7e9023b70c66e125f5ce0b247918c3b
Jisu-Shin <sjszzang3053@gmail.com>
2025-12-28 22:05:32 +0900

    Test : Jpa 저장 및 swimcapMapper 테스트

---
+++ spring-boot/src/test/java/com/swimcolor/repository/JpaSwimcapRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.swimcolor.repository;
+
+import com.swimcolor.domain.Swimcap;
+import com.swimcolor.domain.Swimsuit;
+import com.swimcolor.dto.SwimcapListDto;
+import com.swimcolor.dto.SwimsuitListDto;
+import com.swimcolor.mapper.SwimcapMapper;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class JpaSwimcapRepositoryTest {
+
+    @Autowired
+    JpaSwimcapRepository swimcapRepository;
+
+    private final SwimcapMapper swimcapMapper = Mappers.getMapper(SwimcapMapper.class);
+
+    @Test
+    public void mapper확인() throws Exception {
+        //given
+        Swimcap swimcap = Swimcap.builder().brand("abc")
+                .price(10000)
+                .name("수모")
+                .productUrl("http://~")
+                .imageUrl("http://~")
+                .colors(List.of("#1234","#5678"))
+                .build();
+        swimcapRepository.save(swimcap);
+        Swimcap entity = swimcapRepository.findAll().get(0);
+        System.out.println("swimcap = " + entity);
+
+        //when
+        SwimcapListDto dto = swimcapMapper.toDto(entity);
+        System.out.println("dto = " + dto);
+
+        //then
+        assertThat(dto.getId()).isNotEmpty();
+        assertThat(dto.getColors()).isNotEmpty();
+    }
+}
