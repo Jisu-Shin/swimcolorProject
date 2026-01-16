9eb1c9f6612990b7b9b71a1d6f0ea1bef2e559bd
Jisu-Shin <sjszzang3053@gmail.com>
2026-01-17 01:12:22 +0900

    Chore :[SpringBoot] build.gradle 안쓰는 라이브러리 삭제로 필요없는 import문 제거

--- spring-boot/src/main/java/com/swimcolor/config/RestClientConfig.java
+++ spring-boot/src/main/java/com/swimcolor/config/RestClientConfig.java
@@ -1,13 +1,8 @@
 package com.swimcolor.config;
 
-import org.apache.hc.client5.http.config.ConnectionConfig;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
-import org.apache.hc.core5.util.Timeout;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
