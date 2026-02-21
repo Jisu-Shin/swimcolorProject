490594d02270c382b5f3f23c37c5cac7424e94c2
Jisu-Shin <sjszzang3053@gmail.com>
2026-02-22 03:04:32 +0900

    Update :[SpringBoot] ci test

--- spring-boot/src/main/java/com/swimcolor/dto/RecommendListDto.java
+++ spring-boot/src/main/java/com/swimcolor/dto/RecommendListDto.java
@@ -2,8 +2,10 @@ package com.swimcolor.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class RecommendListDto {
     private String swimsuitId;
