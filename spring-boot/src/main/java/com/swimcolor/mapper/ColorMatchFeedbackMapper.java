a6062c531e42e72a03de8c6c33681cfe0017803c
Jisu-Shin <sjszzang3053@gmail.com>
2026-02-23 16:12:12 +0900

    Update :[SpringBoot] 수영복 이름 글자수 제한

--- spring-boot/src/main/java/com/swimcolor/mapper/ColorMatchFeedbackMapper.java
+++ spring-boot/src/main/java/com/swimcolor/mapper/ColorMatchFeedbackMapper.java
@@ -1,11 +1,7 @@
 package com.swimcolor.mapper;
 
-import com.swimcolor.domain.ColorMatch;
 import com.swimcolor.domain.ColorMatchFeedback;
 import com.swimcolor.dto.ColorMatchFeedbackDto;
-import com.swimcolor.dto.ColorMatchListDto;
-import com.swimcolor.dto.ColorMatchViewDto;
-import com.swimcolor.dto.RecommendListDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
